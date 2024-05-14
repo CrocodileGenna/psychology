e9e91f7773c48e64e31621a658416efc6f6a9f8b
YuriiMoskalenko <99128333+CrocodileGenna@users.noreply.github.com>
2024-05-14 22:42:05 +0300

    slider1

--- src/components/pages/rewiews/Reviews.jsx
+++ src/components/pages/rewiews/Reviews.jsx
@@ -1,74 +1,105 @@
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-
+import { SliderContainer, SliderContent } from './Reviews.styled';
 const Rewiews = () => {
   const testimonials = [
     { id: 1, text: 'Отзыв 1' },
     { id: 2, text: 'Отзыв 2' },
     { id: 3, text: 'Отзыв 3' },
+    { id: 4, text: 'Отзыв 4' },
+    { id: 5, text: 'Отзыв 5' },
+    { id: 6, text: 'Отзыв 6' },
+    { id: 7, text: 'Отзыв 7' },
+    { id: 8, text: 'Отзыв 8' },
     // Добавьте нужное количество отзывов
   ];
 
+  const prevArrowStyle = {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '0',
+    transform: 'translateY(-50%)',
+    zIndex: '999',
+    cursor: 'pointer',
+  };
+
+  const nextArrowStyle = {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    width: '50px',
+    height: '50px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    top: '50%',
+    right: '0',
+    transform: 'translateY(-50%)',
+    zIndex: '999',
+    cursor: 'pointer',
+  };
   const CustomPrevArrow = props => {
-    const { className, style, onClick } = props;
+    const { className, onClick } = props;
     return (
       <div
         className={className}
-        style={{ ...style, display: 'block', background: 'red' }}
+        style={{ ...prevArrowStyle }}
         onClick={onClick}
       >
-        Назад
+        <p>Prev</p>
       </div>
     );
   };
 
   const CustomNextArrow = props => {
-    const { className, style, onClick } = props;
+    const { className, onClick } = props;
     return (
       <div
         className={className}
-        style={{ ...style, display: 'block', background: 'green' }}
+        style={{ ...nextArrowStyle }}
         onClick={onClick}
       >
-        Вперед
+        <p>Next</p>
       </div>
     );
   };
   const settings = {
-    dots: false, // Отключаем пагинацию
+    className: 'center',
+    dots: true, // Отключаем пагинацию
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: 2,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
     centerMode: true, // Включаем режим центрирования
-    centerPadding: '60px', // Добавляем отступы для более четкого центрирования
+    centerPadding: '25%',
     prevArrow: <CustomPrevArrow />,
     nextArrow: <CustomNextArrow />,
+    // autoplay: true,
+    // autoplaySpeed: 3000,
   };
   return (
     <>
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Slider {...settings}>
-        {testimonials.map(testimonial => (
-          <div key={testimonial.id}>
-            <p>{testimonial.text}</p>
-          </div>
-        ))}
-      </Slider>
+      <SliderContainer>
+        <SliderContent>
+          <Slider
+            {...settings}
+            prevArrow={<CustomPrevArrow />}
+            nextArrow={<CustomNextArrow />}
+          >
+            {testimonials.map(testimonial => (
+              <div key={testimonial.id}>
+                <p>{testimonial.text}</p>
+              </div>
+            ))}
+          </Slider>
+        </SliderContent>
+      </SliderContainer>
     </>
   );
 };
